bf040822fada5af504be6fee5dd3b4a6a40ff94a
Evan Carroll <me@evancarroll.com>
2018-12-21 04:37:01 -0600

    initial commit for a master schema

--- sql/10_census_race_ethnicity.sql
+++ sql/10_census_race_ethnicity.sql
@@ -2,6 +2,8 @@
 -- May not be the best idea, but race/ethnicity come from census.
 -- https://www.census.gov/newsroom/press-releases/2018/2020-race-questions.html
 --
+CREATE SCHEMA census;
+
 CREATE TYPE census.race AS ENUM (
 	'American Indian or Alaska Native',
 	'Asian',
